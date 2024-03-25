4a04b6cf99c4d61ef78ee0dbaacc84b148906c33
Phung Nguyen <phung.nguyen@digicommercegroup.com>
2024-03-25 17:18:57 +0700

    init source

---
+++ platformservice/src/test/java/com/example/platformservice/PlatformserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
